1dbdacecd0bf67e9741779a5cb3e031df15b2c15
IvanSusano <ivan.susano16@tectijuana.edu.mx>
2020-10-21 17:11:54 -0700

    Practices Tuple

---
+++ Practices/Practices_1/Tuple.scala
@@ -0,0 +1,11 @@
+//given the tuple (2,4,5,1,2,3,3.1416,23) return the number 3.1416
+
+//Creat a tuple 
+val tuple = (2,4,5,1,2,3,3.1416,23)
+
+//Print the elements we are loocking for 
+println(tuple._7)
+
+//The result is 
+//scala> println(tuple._7)
+//3.1416
