5614e7ed286e554efa542de6fba471556a52b48c
IvanSusano <ivan.susano16@tectijuana.edu.mx>
2020-10-21 18:53:06 -0700

    Practices2 List2

---
+++ Practices/Practices_2/List.scala
@@ -0,0 +1,7 @@
+//Create one list call "lista" with elements "rojo","blanco","negro"
+
+//Creat list
+val lista = List("rojo","blanco","negro")
+
+//The result is 
+//lista: (String, String, String) = (rojo,blanco,negro)
---
+++ Practices/Practices_2/List2.scala
@@ -0,0 +1,22 @@
+//Add 5 more elements to "Lista" "Verde","Amarillo","Azul","Naranja","Perla"
+
+//The list that we are going to add the new elements
+//val lista = List("rojo","blanco","negro")
+
+//Adding element 1 "Verde"
+val ele = "Verde" :: lista
+
+//Adding element 2 "Amarillo"
+val ele = "Amarillo" :: ele
+
+//Adding element 3 "Azul"
+val ele3 = "Azul" :: ele2 
+
+//Adding element 4 "Naranja"
+val ele4 = "Naranja" :: ele3
+
+//Adding element 5 "Perla"
+val ele5 = "Perla" :: ele4 
+//The result is 
+//scala> ele5
+//res5: List[String] = List(Perla, Naranja, Azul, amarillo, julia, rojo, blanco, negro)
