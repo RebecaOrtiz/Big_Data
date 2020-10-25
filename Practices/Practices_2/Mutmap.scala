cd9298fad34b714400a70f988785506b0b5c3ef1
Rebeca Ortiz <rebeca@rebeca>
2020-10-24 21:08:51 -0700

    Exercise number 6 of practice 2 was created and carried out

---
+++ Practices/Practices_2/Mutmap.scala
@@ -0,0 +1,8 @@
+//6.- A mutable map called names is created with the following data
+val names=collection.mutable.Map(("Jose",20),("Luis",24),("Ana",23),("Susana",27))
+
+//6a.- All keys on the map are printed
+names.Keys
+
+//6b.- The following value is added to the map
+mutmap += ("Miguel" -> 23)
