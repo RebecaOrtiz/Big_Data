ce7b3287e538ac099b49c6777aff22cd235e0743
Rebeca Ortiz <rebeca@rebeca>
2020-10-24 20:56:11 -0700

    Exercise number 4 was created and performed

---
+++ Practices/Practices_1/Slice.scala
@@ -0,0 +1,6 @@
+//The variable is declared with the corresponding phrase
+var mensaje = "Hola Luke yo soy tu padre!"
+
+//The variable is called together with the slice function
+//Inside the parentheses, the starting and ending positions of the word that we want to extract are located
+mensaje slice(5,9)
