903111cf7f522eb647b17b8b7bf1875b3be6cf9a
Rebeca Ortiz <rebeca.ortiz@tectijuana.edu.mx>
2020-10-27 21:48:23 -0700

    Exercise 2 of fibonacci was carried out

---
+++ Practices/Practices_4/fibonacci2.scala
@@ -0,0 +1,21 @@
+//Create function 
+def fibonacci(num:Int):Float=
+{
+//If the condition (if num is less than 2) is met the code will be executed
+    if(num<2)
+    {
+//Returns the value of num          
+        return num
+    }
+//If not     
+        else
+    { 
+        fibona =(((1+Math.sqrt(5))/2))
+        fibona2 =(((1-Math.sqrt(5))/2))/Math.sqrt(5))
+    
+    }
+        return fibona2
+}
+fibonacci (13)
+//The result is
+//res2: Float = 233.0
