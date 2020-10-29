299b2745b597706e75d903c3b7c314b9f9fca155
Rebeca Ortiz <rebeca.ortiz@tectijuana.edu.mx>
2020-10-29 00:08:48 -0700

    Practice 5 changed

--- Practices/Practices_5/SparkFunctions.scala
+++ Practices/Practices_5/SparkFunctions.scala
@@ -1,33 +1,33 @@
-#1.- To import the library
+//1.- To import the library
 import org.apache.spark.sql.SparkSession
-// 
+ 
 
-#2.- To create the database where all the functionality hangs
+//2.- To create the database where all the functionality hangs
 val spark = SparkSession.builder().appName(“MiApp”).master(“local”).getOrCreate()
 
-// 3 To read the data 
+//3.- To read the data 
 val wine = spark.read.option("header", "true").option("inferSchema","true")csv("wine.csv")
 
-#4.- To create a dataframe from a text file
+//4.- To create a dataframe from a text file
 val characters = spark.read.json(“/home/vmuser/ficheros/strangersCharacters.json”)
 
-#5.- Function to show the dataframe
+//5.- Function to show the dataframe
 wine.show ()
 
 //6.- Function to show specific data 
 wine.select("country","points").show();
 
-#7.- Function to specifically display column names
+//7.- Function to specifically display column names
 wine.columns
 
-#8.- Function to show existing columns and information about them
+//8.- Function to show existing columns and information about them
 wine.printSchema ()
 
-#9.- To apply a filter on a dataframe
+//9.- To apply a filter on a dataframe
 val children = characters.filter ($. ”age” <13)
 children.show
 
-#10.- To display the first 3 elements of the dataframe
+//10.- To display the first 3 elements of the dataframe
 children.head (3)
 
 //11.- description of the dataset 
